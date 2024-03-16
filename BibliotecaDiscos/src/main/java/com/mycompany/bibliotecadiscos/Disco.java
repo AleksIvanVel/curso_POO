07fd82dbea22b211a4442d58749497c2859bb320
Aleks Ivan Velazquez <velalmanez@gmail.com>
2024-03-15 21:48:11 -0600

    creacion de proyecto

---
+++ BibliotecaDiscos/src/main/java/com/mycompany/bibliotecadiscos/Disco.java
@@ -0,0 +1,10 @@
+package com.mycompany.bibliotecadiscos;
+
+public class Disco {
+    private short tipodisco;
+    private String nombre;
+    private int anio;
+    private int activas;
+    private int permitidas;
+    
+}
